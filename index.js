9bfccc763cde5442617546ca445bb7b90a5ed086
JavierDuarteC <javierandresduarte@gmail.com>
2019-07-30 20:58:31 -0500

    MongoDB connection and fetch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var path = require('path')
+var mongodb = require('mongodb')
 
 var app = express()
 app.use(express.static('/'))
@@ -12,4 +13,15 @@ app.get('/', function (req, res) {
 })
 app.get('/bundle.js', function (req, res) {
     res.sendFile(path.join(__dirname, 'bundle.js'))
+})
+
+app.get('/dataFromDB', function (req, res) {
+    mongoURL = 'mongodb://localhost:27017/database_first_node';
+    mongodb.connect(mongoURL, function (err, db) {
+        db.collection('example').find().toArray(function (err, doc) {
+            if(err) throw err;  
+            res.json(doc);
+            db.close();
+        });
+    });
 })
